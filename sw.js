5030e8d50de1d535cae748eb998405d6949b9118
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-07-17 13:40:25 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v3-1-1-class-focus';
+const CACHE_NAME='danbridge-v3-2-ai-dashboard';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
