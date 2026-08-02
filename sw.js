e840a920b0c023864494df6c9b93c51543199cfc
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-02 12:51:26 +0800

    V18.18.1 fix mobile undo notification position

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v1818-notion-compact-month';
+const CACHE_NAME='danbridge-v18181-mobile-undo-position';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
