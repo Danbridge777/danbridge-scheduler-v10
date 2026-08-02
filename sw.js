04f559ebb06f3368fbe1dbf653cc24ad703e3326
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-02 23:31:02 +0800

    V18.21.6 strengthen Danbridge brand hierarchy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v18215-seamless-feather-auth';
+const CACHE_NAME='danbridge-v18216-brand-hierarchy-auth';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
