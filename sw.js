322b326ff2fdf16ca919ac1b6b41a6dd85b32f69
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-03 00:10:37 +0800

    V18.21.11 rebalance brand and tagline scale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v182110-bold-shadow-wordmark-auth';
+const CACHE_NAME='danbridge-v182111-brand-scale-auth';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
