662c4aa04d9764709bf395cb9ee7d7daedc525a5
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-02 19:59:05 +0800

    V18.19 add responsive workflow conveniences

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v18181-mobile-undo-position';
+const CACHE_NAME='danbridge-v1819-responsive-convenience';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
