c8a8c3d20672700322966a7daa118ae65f602ae6
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-02 22:26:50 +0800

    V18.20.2 export next week when base date is Sunday

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v18201-weekly-apple-calendar-cursor';
+const CACHE_NAME='danbridge-v18202-sunday-next-week-export';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
