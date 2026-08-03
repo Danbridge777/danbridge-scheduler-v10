6b96576ef8d80b8d55d23deb5cf2116b1360d178
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-03 20:38:15 +0800

    V18.22.1 limit teacher gaps to one week

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v1822-usability-polish';
+const CACHE_NAME='danbridge-v18221-teacher-gap-week';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
