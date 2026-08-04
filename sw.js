ad48219142176875cee83fd9452fec588804bc7a
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-04 10:09:55 +0800

    V20.0.2 harden scheduling recommendations and undo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v20-scheduling-efficiency-1';
+const CACHE_NAME='danbridge-v20-scheduling-efficiency-2';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
