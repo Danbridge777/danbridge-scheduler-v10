52efaf0f7a7f556eacee9715c0265dc182403c5e
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-04 23:28:53 +0800

    V20.0.4 apply Kai typeface globally

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v20-scheduling-efficiency-3';
+const CACHE_NAME='danbridge-v20-scheduling-efficiency-4';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
