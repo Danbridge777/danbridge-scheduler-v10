0a6bc8c61bba6f76a7ade4afe539b2b2d1438fe8
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-04 12:39:14 +0800

    V20.0.3 align gold today marker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v20-scheduling-efficiency-2';
+const CACHE_NAME='danbridge-v20-scheduling-efficiency-3';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
